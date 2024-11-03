ca23f4a3ebd0a0baed2bb13eaa567eaddd1b34a2
NexusGk Softwares <nexusgksoftwares@gmail.com>
2024-11-03 19:24:42 +0300

    "Added cart array and addToCart function to app.js"

--- assets/js/app.js
+++ assets/js/app.js
@@ -2,3 +2,10 @@ document.addEventListener("DOMContentLoaded", function() {
     console.log("Jumia Clone is ready!");
     // Add more functionality here as needed
 });
+let cart = [];
+
+function addToCart(product) {
+    cart.push(product);
+    console.log(`${product.name} added to cart!`);
+    // You can also update the cart display here
+}
